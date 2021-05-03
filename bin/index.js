fe5f756dec89e1fbe1f8d8dbaf3f5f4e7b0d667e
Ivan <antipchel@gmail.com>
2021-05-03 12:17:39 +0300

    stage2

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ import genDiff from '../src/index.js';
 
 const program = new Command();
 program.version('1.0.0')
+  .name("gendiff")
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
