a728bc068546c043021ed06bc2c8cc8961aad7ed
Ivan <antipchel@gmail.com>
2021-05-03 20:38:24 +0300

    sorted

--- src/formatters/default.js
+++ src/formatters/default.js
@@ -13,38 +13,38 @@ visual.genFinish = (tabLevel) => visual.genStr(tabLevel, '}');
 // required rewrite
 JSON.stringify2 = (tabLevel, obj) => {
   let result = visual.genStart();
-  for (const [k, v] of Object.entries(obj)) {
+  for (const [k, v] of Object.entries2(obj)) {
     if (typeof v === 'object' && v !== null) // todo
     { result += JSON.stringify2(tabLevel + 1, v); } else result += visual.genStr(tabLevel + 1, `${k}: ${v}`);
   }
   return result + visual.genFinish(tabLevel);
 };
 Object.entries2 = (obj) => {
-  return _.sortBy(obj.entries(),(a) => a[0]);
+  return _.sortBy(Object.entries(obj),(a) => a[0]);
 }
 
 visual.object_sign = (tabLevel, k, v, sign) => {
   const vRE = (sign === '+') ? v.valueAfter : v.valueBefore;
-  return visual.genStr(tabLevel, ` ${sign} ${k}: ${JSON.stringify2(tabLevel + 1, vRE)}`);
+  return visual.genStr(tabLevel, `  ${sign} ${k}: ${JSON.stringify2(tabLevel + 1, vRE)}`);
 };
 
 visual.object_created = (tabLevel, k, v) => visual.object_sign(tabLevel, k, v, '+');
-visual.created = (tabLevel, k, v) => visual.genStr(tabLevel, ` + ${k}: ${v.valueAfter}`);
+visual.created = (tabLevel, k, v) => visual.genStr(tabLevel, `  + ${k}: ${v.valueAfter}`);
 
 visual.object_deleted = (tabLevel, k, v) => visual.object_sign(tabLevel, k, v, '-');
-visual.deleted = (tabLevel, k, v) => visual.genStr(tabLevel, ` - ${k}: ${v.valueBefore}`);
+visual.deleted = (tabLevel, k, v) => visual.genStr(tabLevel, `  - ${k}: ${v.valueBefore}`);
 
 visual.object_unchanged = (tabLevel, k, v) => visual.genStr(tabLevel + 1, `${k}: ${JSON.stringify2(tabLevel + 1, v.valueAfter)}`);
-visual.unchanged = (tabLevel, k, v) => visual.genStr(tabLevel, `   ${k}: ${v.valueAfter}`);
+visual.unchanged = (tabLevel, k, v) => visual.genStr(tabLevel, `    ${k}: ${v.valueAfter}`);
 
 visual.object_changed = (tabLevel, k, v) => visual.genStr(tabLevel + 1, `${k}: ${visual.ize(v.changedChild, tabLevel + 1)}`);
 visual.object_changed_1 = (tabLevel, k, v) => visual.object_deleted(tabLevel, k, v) + visual.created(tabLevel, k, v);
 visual.object_changed_2 = (tabLevel, k, v) => visual.deleted(tabLevel, k, v) + visual.object_created(tabLevel, k, v);
 visual.changed = (tabLevel, k, v) => visual.deleted(tabLevel, k, v) + visual.created(tabLevel, k, v);
 
-visual.ize = (difs, tabLevel = 0) => {
+visual.ize = (difs, tabLevel = 0) => { 
   let result = visual.genStart();
-  for (const [k, v] of _.sortBy(Object.entries(difs),(a) => a[0])) {
+  for (const [k, v] of Object.entries2(difs)) {
     if (visual[v.dif] === undefined) throw new Error(`buildDif().dif: ${v.dif}; is not supported`);
     result += visual[v.dif](tabLevel, k, v);
   }
