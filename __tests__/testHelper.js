5f58e757b4b593bfd660fc3cc8a5dc7e67675204
Ivan <antipchel@gmail.com>
2021-08-25 20:01:39 +0300

    refactoring 4 codeclimate

--- __tests__/testHelper.js
+++ __tests__/testHelper.js
@@ -3,86 +3,83 @@ import fs from 'fs';
 import _ from 'lodash';
 import dif from '../src/index';
 
+// helpers for parseString
 const splitStr = (str) => str.split(/[\r\n]/);
 const trimStrings = (strs) => strs.map((str) => str.trim());
 const filterStrings = (strs) => strs.filter((str) => str.length > 0);
 const strToArray = (str) => filterStrings(trimStrings(splitStr(str)));
 
-// it will parse JSON into easy format
-const stringHas = (text) => strToArray(text).map((str) => {
-  const preresult1 = {
-    str,
-    start: (str === '{') ? '{' : undefined,
-    finish: (str === '}') ? '}' : undefined,
-  };
-  if (preresult1.start || preresult1.finish) return preresult1;
-
-  const preresult2 = {
-    deleted: (str[0] === '-') ? str : undefined,
-    created: (str[0] === '+') ? str : undefined,
-  };
-  const plusORminus = (preresult2.deleted || preresult2.created) ? 1 : 0;
-
-  const colon = str.search(':');
-  if (colon === -1) throw new Error(': colon not found');
-  const preresult3 = {
-    colon: str[colon],
-    key: str.slice(plusORminus, colon).trim(),
-  };
-  if (preresult3.key.length === 0) throw new Error('key not found');
-
-  const preresult4 = {
-    hash: () => (preresult2.deleted ?? '')
-      + (preresult2.created ?? '')
-      + preresult3.key,
-    afterColon: str.slice(colon + 1).trim(),
-  };
-
-  const startsChildren = (preresult4.afterColon === '}');
-  const preresult5 = {
-    startsChildren,
-    value: (startsChildren === true) ? undefined : preresult4.afterColon,
-  };
-  return _.merge(preresult1,
-    _.merge(preresult2,
-      _.merge(preresult3,
-        _.merge(preresult4, preresult5))));
+const parseBraces = (preresult) => _.merge(preresult, {
+  start: (preresult.str === '{') ? true : false,
+  finish: (preresult.str === '}') ? true : false,
 });
 
-// it will parse stringHas result, to even easier format
-const toPaths = (stringHasReturn) => {
-  const result = stringHasReturn.reduce((acc, str) => {
-    if (str.finish !== undefined) {
-      return { result: acc.result, path: acc.path.slice(0, -1) };
-    }
+const parseSign = (preresult) => {
+  const deleted = (preresult.str[0] === '-');
+  const created = (preresult.str[0] === '+');
+  const deletedORcreated = (deleted || created);
+  const sign = (deletedORcreated) ? preresult.str[0] : '';
+  const str = (deletedORcreated) ? preresult.str.substring(1).trim() : preresult.str;
 
-    if (str.hash === undefined) {
-      return acc;
-    }
-
-    if (str.startsChildren === true) {
-      return { result: acc.result, path: acc.path.concat(str.key) };
-    }
-
-    const basePath = acc.path.join('.');
-    if (basePath.length > 0) {
-      const newKey = `${basePath}.${str.hash()}`;
-      return { result: { ...acc.result, [newKey]: str.value }, path: acc.path };
-    } 
-    
-    return { result: { ...acc.result, [str.hash()]: str.value }, path: acc.path };
-  }, { result: {}, path: [] });
-  return result.result;
+  return _.merge(preresult, { deleted, created, deletedORcreated, sign, str });
 };
+
+const parseColon = (preresult) => {
+  const colonPos = preresult.str.search(':');
+  if (colonPos === -1) throw new Error(': colon not found');
+
+  const key = preresult.str.slice(0, colonPos).trim();
+  if (key.length === 0) throw new Error('key not found');
+  
+  const value = preresult.str.slice(colonPos + 1).trim();
+
+  const children = (value === '{');
+
+  const hash = preresult.sign + key;
+
+  return _.merge(preresult, { key, value, children, hash });
+}
+
+// it will parse default programm answer (required for testing)
+const parseString = (text) => strToArray(text).map((str) => {
+  // scan for braces
+  const braces = parseBraces({ str });
+  if (braces.start || braces.finish) return braces;
+
+  // scan for plus or minus at start
+  const sign = parseSign(braces);
+  
+  // scan for key, value, children and hash
+  const colon = parseColon(sign);
+  
+  return colon;
+});
+
+// it will generate paths from parsedStrings, required to compare results
+const generatePaths = (parsedStrings) => parsedStrings.reduce((acc, parsedString) => {
+  // object started > ignore
+  if (parsedString.start) return acc;
+  // object finished > remove last path variable from array
+  if (parsedString.finish)
+    return { result: acc.result, path: acc.path.slice(0, -1) };
+
+  // inside of braces, add value to result
+  const resultKey = `${parsedString.sign}${acc.path.concat(parsedString.key).join('.')}`;
+  const resultValue = parsedString.value;
+  const resultPath = (parsedString.children) ? acc.path.concat(parsedString.key) : acc.path;
+
+  return { result: { ...acc.result, [resultKey]: resultValue }, path: resultPath };
+}, { result: {}, path: [] }).result
 
 // DRY
 export const testMe = (name, beforeFile, afterFile, expectedFile) => {
   test(name, () => {
     const result = dif(beforeFile, afterFile);
-    const transformedResult = toPaths(stringHas(result));
+    const transformedResult = generatePaths(parseString(result));
+    console.log(generatePaths(parseString(result)))
 
     const expected = fs.readFileSync(expectedFile, 'utf-8');
-    const transformedExpected = toPaths(stringHas(expected));
+    const transformedExpected = generatePaths(parseString(expected));
 
     expect(transformedResult).toEqual(transformedExpected);
   });
